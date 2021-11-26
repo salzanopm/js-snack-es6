d12235bc5abeb2d9e514231130f96ee8dd125655
salzanopm <salzanopm@email.com>
2021-11-26 18:14:10 +0100

    snack 1

---
+++ snack1/js/script.js
@@ -0,0 +1,82 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal nel DOM
+// Snack2
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà:
+// Punti fatti e falli subiti.
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+
+const bikes = [
+    {
+        nome:'Alan',
+        peso: 1000
+    },
+    {
+        nome:'Atala',
+        peso: 2500
+    },
+    {
+        nome:'Basso',
+        peso: 3670
+    },
+    {
+        nome:'Battaglin',
+        peso: 1230
+    },
+    {
+        nome:'BH',
+        peso: 2999
+    },
+    {
+        nome:'Bixxis',
+        peso: 3390
+    }
+]
+
+
+// Creo variabile bicicletta più leggera
+let currentLightBike = bikes[0];
+
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal nel DOM
+
+
+// creo ciclo for fino alla fine dell'array
+for (let i = 0; i < bikes.length; i++) {
+
+    // variabile interna 
+    let thisBike = bikes[i];                
+
+    // se variabile interna che percorre tutto l'array incontra una bici
+    // dal peso minore allora viene riassegnato il valore della bici più leggera
+    if(thisBike.peso < currentLightBike.peso) {
+        currentLightBike = thisBike;
+    }
+    // controllo in console se stampa oggetti correttamente
+    console.log(thisBike)
+}    
+
+// versione con odestructuring prendo variabili e le associo direttamente ai valori senza dover scrivere il nome dell'oggetto con la dot notation dentro al template literal (sugar sintax)
+const {nome,peso} = currentLightBike;
+
+// appendo template literal nell'html 
+const bikesHtml = `
+
+
+
+<h2>Ecco la bici più leggera</h2>
+
+<div>
+    La bicicletta della Marca "${nome}" è la più leggera in quanto pesa solo: ${peso} grammi.
+</div>
+`
+
+// dichiaro variabile container nell'html per appenderci il mio template
+const bikeContainer = document.getElementById ('bike-container')
+  bikeContainer.innerHTML += bikesHtml;
