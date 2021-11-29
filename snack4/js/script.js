031e1278ce2f15018de0687f6f46aacb2b95d1a0
salzanopm <salzanopm@email.com>
2021-11-29 17:38:48 +0100

    snack4 parte 1

--- snack4/js/script.js
+++ snack4/js/script.js
@@ -14,5 +14,21 @@
 // 3-  Dobbiamo creare un nuovo array di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
 
 
-// ANALISI
+// ANALISI es. 1
 
+// [x] copio array
+let students = [
+        { name: 'Marco', id: 213, grades: 78 },
+        { name: 'Paola', id: 110, grades: 96 },
+        { name: 'Andrea', id: 250, grades: 48 },
+        { name: 'Gaia', id: 145, grades: 74 },
+        { name: 'Luigi', id: 196, grades: 68 },
+        { name: 'Piero', id: 102, grades: 50 },
+        { name: 'Francesca', id: 120, grades: 84 },
+      ];
+// [] creo nuovo array contenente nomi in maiuscolo
+const newStudents = students.map((element,index,array) => {
+    return element.name.toUpperCase();
+});
+// stampo il risultato
+console.log(newStudents);
